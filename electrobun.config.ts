dc60f7b59c1cafaee7c8c6d37e1480826260a319
fawzan saeed <fauzdasoodais@gmail.com>
2026-04-17 00:57:06 +0300

    chore: bump version to 1.0.0

--- electrobun.config.ts
+++ electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
 	app: {
 		name: "FluxDL",
 		identifier: "fluxdl.electrobun.dev",
-		version: "0.0.1",
+		version: "1.0.0",
 	},
 	build: {
 		// Vite builds to dist/, we copy from there
